e75b509a415360028e1e6fca865e06f44e343231
Jonas Myhr Refseth <151346@student.nord.no>
2017-10-07 15:38:29 +0200

    More Fighting UI stuff

---
+++ Assets/Scripts/Midterms/FightChampion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FightChampion : MonoBehaviour {
+	public int maxHealth = 1000;
+	public int currentHealth = 1000;
+
+	public Slider healthBar;
+	public Text healthText;
+
+	private int tempHealth = 1000;
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+		string health = currentHealth + "/" + maxHealth;
+		healthBar.maxValue = maxHealth;
+		currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+		
+		tempHealth = Mathf.RoundToInt(Mathf.Lerp((float) tempHealth, (float) currentHealth, .1f));
+		healthBar.value = tempHealth;
+		if(healthText.text != health)
+			healthText.text = health;
+	}
+
+	public void dealDamage(int dmg) {
+		currentHealth -= dmg;
+	}
+}
---
+++ Assets/Scripts/Midterms/MidtermAbility.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MidtermAbility : MonoBehaviour {
+	public GameObject tooltip;
+
+	void Start() {
+		tooltip.SetActive(false);
+	}
+	
+	void Update() {
+		
+	}
+
+	public void OnHoverEnter() {
+		tooltip.SetActive(true);
+	}
+
+	public void OnHoverExit() {
+		tooltip.SetActive(false);
+	}
+}
