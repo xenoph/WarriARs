3c8692774ba703d7c8a58f67b7279edbd939bfb3
Jonas Myhr Refseth <151346@student.nord.no>
2017-10-11 12:30:10 +0200

    Added a flashing healthbar, when the hp is below 1/4th

--- Assets/Scripts/Midterms/FightChampion.cs
+++ Assets/Scripts/Midterms/FightChampion.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class FightChampion : MonoBehaviour {
 	public int maxHealth = 1000;
 	public int currentHealth = 1000;
+	[Range(0, 100)]
+	public int lowPercent = 25;
 
 	public MidtermFightSceneController MFSC;
 
@@ -13,16 +15,39 @@ public class FightChampion : MonoBehaviour {
 	public int ChanceToHit = 100;
 
 	private int tempHealth = 1000;
+	private Image healthFill;
+	private Color healthBarColor;
+	private float healthBlinkDuration = 0.40f;
+	private float colorLerpTime = 0f;
 
 	void Start() {
-		
+		if(healthBar != null) {
+			healthFill = healthBar.fillRect.GetComponent<Image>();
+			healthBarColor = healthFill.color;
+		}
+			
 	}
 	
 	void Update() {
 		string health = currentHealth + "/" + maxHealth;
 		healthBar.maxValue = maxHealth;
 		currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-		
+		float percent = ((float) currentHealth / (float) maxHealth) * 100f;
+
+		if(percent <= lowPercent) {
+			if(colorLerpTime < 1f) {
+				healthFill.color = Color.Lerp(healthBarColor, new Color(healthBarColor.r, healthBarColor.g, healthBarColor.b, 0f), colorLerpTime);
+				colorLerpTime += Time.deltaTime / healthBlinkDuration;
+			} else if(colorLerpTime > 1f && colorLerpTime < 2f) {
+				healthFill.color = Color.Lerp(healthBarColor, new Color(healthBarColor.r, healthBarColor.g, healthBarColor.b, 0f), 2f - colorLerpTime);
+				colorLerpTime += Time.deltaTime / healthBlinkDuration;
+				if(colorLerpTime >= 2f)
+					colorLerpTime -= 2f;
+			}
+		} else {
+			healthFill.color = healthBarColor;
+		}
+
 		tempHealth = Mathf.RoundToInt(Mathf.Lerp((float) tempHealth, (float) currentHealth, .1f));
 		healthBar.value = tempHealth;
 		if(healthText.text != health)
