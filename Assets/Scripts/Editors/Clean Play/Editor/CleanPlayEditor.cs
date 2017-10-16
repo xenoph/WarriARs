6422cea2dad2688d3170a24a73ab3a278dec7e9d
Jonas Myhr Refseth <151346@student.nord.no>
2017-10-16 11:39:02 +0200

    Clean play now loads the main scene on play, if the window is open

--- Assets/Scripts/Editors/Clean Play/Editor/CleanPlayEditor.cs
+++ Assets/Scripts/Editors/Clean Play/Editor/CleanPlayEditor.cs
@@ -4,11 +4,8 @@ using UnityEditor;
 using System.Collections.Generic;
 
 public class CleanPlayEditor : EditorWindow {
-
-    private bool lastPlay = false;
-
-    private Dictionary<string, bool> enabledScenes = new Dictionary<string, bool>();
     private static CleanPlayEditor window;
+	private bool lastPlay = false;
 
     [MenuItem("Window/Clean Play")]
     static void Init() {
@@ -18,35 +15,12 @@ public class CleanPlayEditor : EditorWindow {
 
     void OnGUI() {
         GUILayout.Label("Clean Play", EditorStyles.boldLabel);
-        /*
-        if(EditorApplication.isPlaying) {
-            GUILayout.Label("Cannot be changed in Play Mode");
-        } else {
-            for(int i = 0; i < SceneManager.sceneCount; i++) {
-                string name = SceneManager.GetSceneAt(i).name;
-                bool enabled = true;
-                if(enabledScenes.ContainsKey(name)) {
-                    enabledScenes.TryGetValue(name, out enabled);
-                } else {
-                    enabledScenes.Add(name, enabled);
-                }
-                enabled = EditorGUILayout.Toggle(name, enabled);
-                enabledScenes[name] = enabled;
-            }
-        }
-        */
-        //EditorGUILayout.EndToggleGroup();
     }
 
     void Update() {
         if(EditorApplication.isPlaying) {
             if(lastPlay != EditorApplication.isPlaying) {
-                for(int i = 0; i < SceneManager.sceneCount; i++) {
-                    string name = SceneManager.GetSceneAt(i).name;
-                    if(name != "main") {
-                        SceneManager.UnloadSceneAsync(name);
-                    }
-                }
+				SceneManager.LoadSceneAsync("main");
                 lastPlay = EditorApplication.isPlaying;
             }
         } else {
