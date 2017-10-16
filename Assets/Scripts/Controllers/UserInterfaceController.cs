0010a7cdc6a4657edaf47c0324b5a63300620d27
Bjørn Vidar Dahle <bvidar@dahle.me>
2017-10-16 12:53:06 +0200

    initial battle scene work

---
+++ Assets/Scripts/Controllers/UserInterfaceController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterfaceController : MonoBehaviour {
+
+	public GameObject MapCanvas;
+	public GameObject BattleCanvas;
+
+	void Start () {
+		
+	}
+}
