fc8a3c66e8ceae33bab5079c28dc73f17a9e37c9
Jonas Myhr Refseth <jonas@oisann.net>
2017-11-07 17:51:55 +0100

    Made a build with all scenes in the build scene list, instead of manually entering the scenes

---
+++ Assets/Scripts/Editors/CI Build/Editor/AutoBuilder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AutoBuilder : MonoBehaviour {
+
+    [MenuItem("Auto Builder/Android")]
+    static void BuildNightlyAndroid() {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        List<string> scenes = new List<string>();
+        foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+            scenes.Add(scene.path);
+        }
+
+        string date = DateTime.Now.ToString("yyyy") + DateTime.Now.ToString("MM") + DateTime.Now.ToString("dd"); // + DateTime.Now.ToString("HH") + DateTime.Now.ToString("mm") + DateTime.Now.ToString("ss");
+
+        buildPlayerOptions.scenes = scenes.ToArray();
+        buildPlayerOptions.locationPathName = "WarriARs-NIGHTLY-" + date + ".apk";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
