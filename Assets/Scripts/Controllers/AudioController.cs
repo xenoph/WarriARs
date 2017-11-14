dd704420ac946d1857fbbbec105e6d1adc589e4a
Bjørn Vidar Dahle <bvidar@dahle.me>
2017-11-14 13:33:58 +0100

    swapping audio

---
+++ Assets/Scripts/Controllers/AudioController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	public AudioSource MainAudioSource;
+	public AudioSource BattleAudioSource;
+
+	public float FadeTimer;
+
+	private bool _fadeBattleOut;
+	private bool _fadeMainOut;
+
+	private float _startVolume;
+
+	private AudioSource _currentAudio;
+
+	public void SwitchAudioSource() {
+		StartCoroutine (SwapAudio ());
+	}
+
+	private IEnumerator SwapAudio() {
+			_startVolume = _currentAudio.volume;
+			while (_currentAudio.volume > 0) {
+				_currentAudio.volume -= _startVolume * Time.deltaTime / FadeTimer;
+				yield return null;
+			}
+
+			_currentAudio.Stop ();
+			_currentAudio.volume = _startVolume;
+			if (_currentAudio == MainAudioSource) {
+				_currentAudio = BattleAudioSource;
+
+			} else {
+				_currentAudio = BattleAudioSource;
+			}
+			_currentAudio.Play ();
+	}
+}
