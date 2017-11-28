ec1b9f26a8c059065e9988d7dd87225fa8990eef
Adrian Gulseth Killi <adriangkilli@hotmail.com>
2017-11-28 12:52:24 +0100

    made a script that changes skybox

---
+++ Assets/Scripts/SkyboxChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChange : MonoBehaviour {
+
+	public Material skobyx;
+
+	// Use this for initialization
+	void Start () {
+		RenderSettings.skybox = skobyx;
+	}
+	
+	// Update is called once per frame
+	void OnDisable () {
+
+		RenderSettings.skybox = null;
+		
+	}
+}
