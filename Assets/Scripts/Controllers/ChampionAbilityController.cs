691bca499e48404dfc1dbe5041febc04554f4292
Bjørn Vidar Dahle <bvidar@dahle.me>
2017-11-28 20:19:16 +0100

    made a quick ability sound picker / player

--- Assets/Scripts/Controllers/ChampionAbilityController.cs
+++ Assets/Scripts/Controllers/ChampionAbilityController.cs
@@ -10,7 +10,19 @@ public class ChampionAbilityController : MonoBehaviour {
 	public ParticleSystem Ability2Effect;
 	public ParticleSystem Ability3Effect;
 
+	private AudioSource _aSource;
+
+	private void Awake() {
+		_aSource = GetComponent<AudioSource>();
+	}
+
 	public void PlayAbilityEffect(int effect) {
+		var clip = GetRandomSound();
+		if(clip != null) {
+			_aSource.clip = clip;
+			_aSource.Play();
+		}
+
 		switch (effect) {
 			case 0:
 				Ability1Effect.Play();
@@ -31,5 +43,11 @@ public class ChampionAbilityController : MonoBehaviour {
 
 	private void PlayAnimation(int abNumber) {
 		if(AbilityEffectAnimator == null) { return; }
+	}
+
+	private AudioClip GetRandomSound() {
+		var allSounds = Resources.LoadAll("Sounds/AbilityVFX", typeof(AudioClip));
+		AudioClip sound = allSounds[Random.Range(0, allSounds.Length)] as AudioClip;
+		return sound;
 	}
 }
